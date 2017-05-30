696bf09b8b9df2bb4872c6b0d0b94c4985951b3c
Francis Rivera <Francis@Astronymphy.local>
2017-05-30 14:17:44 -0400

    Added my first bash loop

---
+++ sample_bash_script/loop.sh
@@ -0,0 +1,20 @@
+## This is a sample loop in bash
+
+#for i in 1 2:
+#do 
+#echo $i;
+
+#for j in a b;
+#do
+#echo $j;
+#done
+
+#done
+for k in {1..10};
+do
+echo $k;
+for h in {a..g};
+do
+echo $h;
+done
+done
